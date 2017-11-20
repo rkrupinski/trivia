9cbd65d63ff71e5b6844bb5f25eb4cd0d26ed0be
Rafał Krupiński <krupinski.rafal@gmail.com>
2017-11-20 15:14:53 +0100

    Does not work.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,8 +1,42 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Navigation
 
 
-main : Html msg
+type Msg
+    = UrlChange Navigation.Location
+
+
+type alias Model =
+    {}
+
+
+main : Program Never Model Msg
 main =
-    text "Trivia"
+    Navigation.program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    {} ! []
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    model ! []
+
+
+view : Model -> Html Msg
+view model =
+    p [] [ text "Trivia" ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
