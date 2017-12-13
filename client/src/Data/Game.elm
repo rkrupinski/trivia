356efede55932d3ccd6c0c6b76505cb3ad873e89
Rafał Krupiński <krupinski.rafal@gmail.com>
2017-12-13 22:39:50 +0100

    No time to commit.. My people need me!

--- client/src/Data/Game.elm
+++ client/src/Data/Game.elm
@@ -15,11 +15,35 @@ type alias GameId =
 
 type alias Game =
     { id : GameId
+    , can_join : Bool
+    , status : Status
     }
+
+
+type Status
+    = Opened
+
+
+statusFromString : String -> Decode.Decoder Status
+statusFromString statusStr =
+    case statusStr of
+        "opened" ->
+            Decode.succeed Opened
+
+        _ ->
+            Decode.fail <| "Invalid status: " ++ statusStr
+
+
+statusDecoder : Decode.Decoder Status
+statusDecoder =
+    Decode.string
+        |> Decode.andThen statusFromString
 
 
 gameDecoder : Decode.Decoder Game
 gameDecoder =
     rootDecoder <|
-        Decode.map Game <|
-            Decode.field "id" Decode.string
+        Decode.map3 Game
+            (Decode.field "id" Decode.string)
+            (Decode.field "can_join" Decode.bool)
+            (Decode.field "status" statusDecoder)
